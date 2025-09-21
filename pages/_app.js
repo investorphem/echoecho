c2a9754b1489a4ce93e4782d3da669afa9b9d9ac
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-09-22 00:18:09 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+import { sdk } from "@farcaster/frame-sdk";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Notify Farcaster miniapp shell that your app is ready
+    sdk.actions.ready();
+  }, []);
+
   return <Component {...pageProps} />;
 }
