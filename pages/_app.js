47e4017312edb128800cde3eeab30d7df4fd03ff
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-09-22 01:48:47 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,48 @@
-import { useEffect } from "react";
-import { sdk } from "@farcaster/miniapp-sdk";
+import { useEffect } from 'react';
+import { sdk } from '@farcaster/miniapp-sdk';
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createConfig, http } from 'wagmi';
+import { base } from 'wagmi/chains';
+import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector';
 
-export default function App({ Component, pageProps }) {
+const config = createConfig({
+  chains: [base],
+  transports: {
+    [base.id]: http(),
+  },
+  connectors: [
+    miniAppConnector(),
+  ],
+});
+
+const queryClient = new QueryClient();
+
+export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    // Notify Farcaster miniapp shell that your app is ready
-    sdk.actions.ready();
+    console.log('Initializing Farcaster SDK in _app.js...');
+    sdk.connectAsync()
+      .then(async () => {
+        const user = await sdk.user.getAsync();
+        console.log('User FID:', user.fid);
+        await sdk.actions.ready();
+        console.log('Farcaster Mini App ready');
+        if (sdk.wallet.isConnected()) {
+          console.log('Wallet connected:', sdk.wallet.address());
+        } else {
+          console.warn('Wallet not connected');
+        }
+      })
+      .catch((error) => {
+        console.error('Farcaster SDK Error:', error);
+      });
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
