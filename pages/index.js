af314a87921bbc38787da49303695ed401f19a5d
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-09-22 14:32:57 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -355,7 +355,7 @@ export default function Home() {
               animation: 'spin 1s linear infinite',
             }}
           />
-          <style jsx>{`
+          <style className>{`
             @keyframes spin {
               0% { transform: rotate(0deg); }
               100% { transform: rotate(360deg); }
