a7537b95165173707c46ef0b1e527ebc60befa21
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-22 09:46:34 +0100

    Update MiniAppSDK.tsx

--- components/MiniAppSDK.tsx
+++ components/MiniAppSDK.tsx
@@ -12,7 +12,7 @@ export default function MiniAppSDK({
   onUser?: (data: any) => void;
 }) {
   useEffect(() => {
-    // 1. THIS 
+    // 1. THIS
     sdk.actions.ready().catch(() => {});
 
     // 2. Optional: get user data safel
