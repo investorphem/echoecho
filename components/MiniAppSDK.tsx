8f50bd7fbee0a1003e13023cb3098292e569dc58
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-22 09:43:21 +0100

    Update MiniAppSDK.tsx

--- components/MiniAppSDK.tsx
+++ components/MiniAppSDK.tsx
@@ -12,7 +12,7 @@ export default function MiniAppSDK({
   onUser?: (data: any) => void;
 }) {
   useEffect(() => {
-    // 1. THIS REMOVES THE SPLASH SCRE
+    // 1. THIS REMOVES THE SPLASH SCR
     sdk.actions.ready().catch(() => {});
 
     // 2. Optional: get user data safel
