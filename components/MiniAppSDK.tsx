c29fe1708b8625ad77f094385656d2b1e1b3acc5
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-22 09:49:35 +0100

    Update MiniAppSDK.tsx

--- components/MiniAppSDK.tsx
+++ components/MiniAppSDK.tsx
@@ -15,7 +15,7 @@ export default function MiniAppSDK({
     // 
     sdk.actions.ready().catch(() => {});
 
-    // 2. Optional: get u
+    // 2. Optional: get 
     const getUser = async () => {
       try {
         const context = await sdk.getLocationContext().catch(() => ({}));
