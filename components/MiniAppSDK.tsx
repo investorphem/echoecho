a9a73e5c005258c522de82279ea6422e85c755b6
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-22 09:44:17 +0100

    Update MiniAppSDK.tsx

--- components/MiniAppSDK.tsx
+++ components/MiniAppSDK.tsx
@@ -12,7 +12,7 @@ export default function MiniAppSDK({
   onUser?: (data: any) => void;
 }) {
   useEffect(() => {
-    // 1. THIS REMOVES THE SPLAS
+    // 1. THIS REMOVES THE SPLA
     sdk.actions.ready().catch(() => {});
 
     // 2. Optional: get user data safel
