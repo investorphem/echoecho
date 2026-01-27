ad843783616c4542791454ee00c375aa22ade435
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-27 18:12:18 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
+  output: 'standaone',
 
   images: 
     domains ['echochos.vecel.app', 'farcaster.xyz', 'warpcast.com'],
