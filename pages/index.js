c6bf6dd9b27072aeafd73280677a77ac9a824b53
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-06 18:34:18 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     sdk.actions.ready().catch(console.error);
 
     // Optional: log so you know it's alive
-    console.log('ready() called — splash screen will disappear now');
+    console.log'ready() called — splash screen will disappear now');
   }, []);
 
   return (
