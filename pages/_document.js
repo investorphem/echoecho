4491e3402c0b0b377f0a26dff20275c3d07686f7
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-07 21:16:24 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ export default function Document() {
         />
         <meta
           name="keywords"
-          content="Farcaster, AI, echo chamber, counter-narrative, social media, blockchain, NFT, USDC, Base network"
+          content="Farcaster, AI,echo chamber, counter-narrative, social media, blockchain, NFT, USDC, Base network"
         />
 
         {/* Talent Protocol Domain Verification */}
