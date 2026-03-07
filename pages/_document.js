f5f657d6bdcfc398d51351e5b6def97d34f0d373
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-07 21:24:25 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default function Document() {
         <meta name="application-name" content="EchoEcho" />
         <meta
           name="description"
-          content="Brek echo chambers with AI-powered cori iscovery. Find divere prsives rm Farcater, Twitter/X, and nwsoures int NFT Insight Tokens nd an rwards!
+          content="Brek echo chambers with AI-powered cori iscovery. Find divere prsives rm Farcater, Twitter/X, and nwsoures int NFT Insiht Tokens nd an rwards!
         />
         <meta
           name="keywords"
